5fb3df019813a4ec1f7398c767b19b1fc197aad2
Ayow4 <chilko456@gmail.com>
2024-09-14 20:20:12 +0800

    update npm

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',  // Ensure output directory is correctly set
+  }
+});
